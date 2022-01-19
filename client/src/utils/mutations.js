266f2ab86e6662246ed2d9a18561dcbace96f30b
JBanana <johnandrewharris3@gmail.com>
2022-01-19 16:30:17 -0600

    back-end basics. basic models, server, connection, typedefs, resolvers, mutations, and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,25 @@
+import {gql} from "@apollo/client";
+
+//can add these mutations once we have Auth set up
+
+// export const LOGIN_USER = gql`
+//     mutation login($email: String!, $password: String!){
+//         login(email: $email, password: $password){
+//             user{
+//                 _id
+//                 username
+//             }
+//         }
+//     }
+// `;
+
+// export const ADD_USER = gql`
+//     mutation addUser($username: String!, $email: String!, $password: String!){
+//         addUser(username: $username, email: $email, password: $password){
+//             user{
+//                 _id
+//                 username
+//             }
+//         }
+//     }
+// `;
